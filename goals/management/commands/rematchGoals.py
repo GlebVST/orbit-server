4d88e75ee3b284592816ba2ec6942801a6587ee1
Faria Chowdhury <faria.chowdhury@gmail.com>
2018-09-19 22:25:04 -0700

    Managment cmd to check for and create usergoals
    Management command: rematchGoals to be run daily on admin server.
    It checks for any new UserGoals to be created, and remove any stale ones
    that no longer apply.
    admin_scripts: remove _staging version of files because we now have a
    proper admin-staging server that is setup like the prod admin server.
    requirements.txt: upgrade reportlab package

---
+++ goals/management/commands/rematchGoals.py
@@ -0,0 +1,23 @@
+import logging
+from django.core.management.base import BaseCommand, CommandError
+from django.conf import settings
+from django.utils import timezone
+from goals.models import UserGoal
+
+logger = logging.getLogger('mgmt.goals')
+
+class Command(BaseCommand):
+    help = "For existing users with UserGoals, call rematchGoals to check for new goals and remove stale goals."
+
+    def handle(self, *args, **options):
+        # get distinct users
+        users = UserGoal.objects.all().values_list('user', flat=True).distinct().order_by('user')
+        total = 0
+        for user in users:
+            new_goals = UserGoal.objects.rematchGoals(user)
+            for ug in new_goals:
+                self.stdout.write("Created UserGoal {0}".format(ug))
+            total += len(new_goals)
+        msg = 'rematchGoals: num new UserGoals created: {0}'.format(total)
+        logger.info(msg)
+        self.stdout.write(msg)
