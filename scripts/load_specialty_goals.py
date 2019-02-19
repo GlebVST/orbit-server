2585bdb2e11a59591ec75e369d21a0b7500fd835
Faria Chowdhury <faria.chowdhury@gmail.com>
2019-02-18 23:45:40 -0800

    Add mgmt command for compute orgUserSnapshot.
    Add admin script that calls this mgmt cmd. This script should be calld
    in crontab on admin server.

--- scripts/load_specialty_goals.py
+++ scripts/load_specialty_goals.py
@@ -7,6 +7,12 @@ from import_state_goals import *
 
 logger = logging.getLogger('mgmt.goals')
 
+# Specialty name  => PracticeSpecialty instance
+specialtyDict = {}
+specs = PracticeSpecialty.objects.all()
+for m in specs:
+    specialtyDict[m.name] = m
+
 #
 # Specialty srcmegoals
 # df2 = df[(~df.Specialties.isnull()) & (~df.Srcme_tag.isnull()) & (~df.Credits.isnull()) & (~df.Interval.isnull())]
@@ -428,7 +434,151 @@ def load_tx_emsdir_srcmegoal():
 
 
 #
-# Exclude_specialty goals
+# Exclude_specialty cme goals
 # df2 = df[(~df.Exclude_specialties.isnull())]
 #
 
+def load_ca_pain_palliative_cmegoals():
+    """Exclude_specialties=radiology, pathology
+    ('CA', 'MD', 'radiology, pathology', nan, 'pain management', '2', 6.0, 'AMA-1, AOA-1A, AOA-1B')
+    ('CA', 'MD', 'radiology, pathology', nan, 'palliative care', '2', 6.0, 'AMA-1, AOA-1A, AOA-1B')
+    """
+    state = statesDict['CA']
+    degrees = [degreeDict['MD'],]
+    creditTypes = [
+            get_creditType('AMA-1'),
+            get_creditType('AOA-1A'),
+            get_creditType('AOA-1B'),
+        ]
+    interval = 2
+    credits = 6
+    specialties = []
+    for specname in specialtyDict:
+        if specname not in ('Radiology','Pathology'):
+            specialties.append(specialtyDict[specname])
+    lg = getStateLicenseGoal(state)
+    tags = [
+        CmeTag.objects.get(name='Pain Management'),
+        CmeTag.objects.get(name='Palliative Care')
+    ]
+    for tag in tags:
+        do_create = True
+        # check if exist
+        fkwargs = {
+            'entityType': CmeGoal.STATE,
+            'state': state,
+            'licenseGoal': lg,
+            'cmeTag': tag,
+            'credits': credits,
+            'goal__interval': interval
+        }
+        qs = CmeGoal.objects.filter(**fkwargs).select_related('goal').order_by('pk')
+        for cg in qs:
+            if (isEqual(degrees, cmegoal.goal.degrees.all())
+                    and isEqual(specialties, cmegoal.goal.specialties.all())
+                    and isEqual(creditTypes, cmegoal.creditTypes.all())):
+                print('Found existing cmegoal: {0}'.format(cg))
+                do_create = False
+        if not do_create:
+            continue
+        # else create basegoal
+        bg_dueDateType = BaseGoal.RECUR_LICENSE_DATE
+        bg = BaseGoal.objects.create(
+            goalType=goaltype_cme,
+            dueDateType=bg_dueDateType,
+            interval=interval,
+            notes=''
+        )
+        for deg in degrees:
+            bg.degrees.add(deg)
+        for spec in specialties:
+            bg.specialties.add(spec)
+        # create cmegoal
+        cmegoal = CmeGoal.objects.create(
+            goal=bg,
+            entityType=CmeGoal.STATE,
+            state=state,
+            cmeTag=tag,
+            licenseGoal=lg,
+            credits=credits,
+        )
+        for m in creditTypes:
+            cmegoal.creditTypes.add(m)
+        msg = "Created {0.state} Specialty CmeGoal {0.pk}|{0}|{0.cmeTag}|{0.credits}|{1}".format(cmegoal, cmegoal.formatCreditTypes())
+        print(msg)
+        logger.info(msg)
+
+
+def load_or_pain_eol_cmegoals():
+    """
+    ('OR', 'MD, DO', 'radiology/teleradiology (OR)', nan, 'End-of-Life Care', 'once', 3.0, 'AMA-1, AOA-1A, AOA-1B')
+    ('OR', 'MD, DO', 'radiology/teleradiology (OR)', nan, 'Pain Management', 'once', 3.0, 'AMA-1, AOA-1A, AOA-1B')
+    """
+    state = statesDict['OR']
+    degrees = [degreeDict['MD'], degreeDict['DO']]
+    creditTypes = [
+            get_creditType('AMA-1'),
+            get_creditType('AOA-1A'),
+            get_creditType('AOA-1B'),
+        ]
+    interval = 0
+    credits = 3
+    specialties = []
+    subspecialties = [] # everything but radiology/teleradiology
+    rad_spec = specialtyDict['Radiology']
+    subspecs = rad_spec.subspecialties.exclude(name='Teleradiology (OR)')
+    for ss in subspecs:
+        subspecialties.append(ss)
+    lg = getStateLicenseGoal(state)
+    tags = [
+        CmeTag.objects.get(name__iexact='Pain Management'),
+        CmeTag.objects.get(name__iexact='End-of-Life Care')
+    ]
+    for tag in tags:
+        do_create = True
+        # check if exist
+        fkwargs = {
+            'entityType': CmeGoal.STATE,
+            'state': state,
+            'licenseGoal': lg,
+            'cmeTag': tag,
+            'credits': credits,
+            'goal__interval': interval
+        }
+        qs = CmeGoal.objects.filter(**fkwargs).select_related('goal').order_by('pk')
+        for cg in qs:
+            if (isEqual(degrees, cmegoal.goal.degrees.all())
+                    and isEqual(specialties, cmegoal.goal.specialties.all())
+                    and isEqual(creditTypes, cmegoal.creditTypes.all())):
+                print('Found existing cmegoal: {0}'.format(cg))
+                do_create = False
+        if not do_create:
+            continue
+        # else create basegoal
+        bg_dueDateType = BaseGoal.ONE_OFF
+        bg = BaseGoal.objects.create(
+            goalType=goaltype_cme,
+            dueDateType=bg_dueDateType,
+            interval=interval,
+            notes=''
+        )
+        for deg in degrees:
+            bg.degrees.add(deg)
+        for spec in specialties:
+            bg.specialties.add(spec)
+        for subspec in subspecialties:
+            bg.subspecialties.add(subspec)
+        # create cmegoal
+        cmegoal = CmeGoal.objects.create(
+            goal=bg,
+            entityType=CmeGoal.STATE,
+            state=state,
+            cmeTag=tag,
+            licenseGoal=lg,
+            credits=credits,
+        )
+        for m in creditTypes:
+            cmegoal.creditTypes.add(m)
+        msg = "Created {0.state} Specialty CmeGoal {0.pk}|{0}|{0.cmeTag}|{0.credits}|{1}".format(cmegoal, cmegoal.formatCreditTypes())
+        print(msg)
+        logger.info(msg)
