94cfbbe8fe9009cdab2a36073e004e1938f08b70
Faria Chowdhury <faria.chowdhury@gmail.com>
2019-07-07 00:19:14 -0700

    Add shell scripts for start/stop celery workers.
    start_celery.sh: start celery daemon
    stop_celery.sh: stop cdlery daemon

---
+++ start_celery.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# http://celery.readthedocs.org/en/latest/reference/celery.bin.multi.html#module-celery.bin.multi
+# From celery docs: Pidfiles and logfiles are stored in the current directory by default.
+#   Use --pidfile and --logfile argument to change this.
+#   The abbreviation %n will be expanded to the current node name.
+#   To stop the worker, need to pass in the same pidfile,logfile as what was used to start it
+#
+# Create 2 workers:
+#   concurrency=1 because t2.small has only 1 core.
+SCRIPT_DIR=$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")
+BASEDIR="${SCRIPT_DIR}/celery"
+celery multi start w1 w2 -A mysite -c1 -l info --pidfile="${BASEDIR}/run/%n.pid" --logfile="${BASEDIR}/log/%n.log"
---
+++ stop_celery.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+SCRIPT_DIR=$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")
+BASEDIR="${SCRIPT_DIR}/celery"
+celery multi stopwait w1 -A mysite --pidfile="${BASEDIR}/run/%n.pid" --logfile="${BASEDIR}/log/%n.log"
+celery multi stopwait w2 -A mysite --pidfile="${BASEDIR}/run/%n.pid" --logfile="${BASEDIR}/log/%n.log"
