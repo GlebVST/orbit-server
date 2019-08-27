3c004574bb852f764d00eec4b180195faed060d4
Faria Chowdhury <faria.chowdhury@gmail.com>
2019-08-26 19:57:41 -0700

    typo fix

--- goals/models.py
+++ goals/models.py
@@ -1102,7 +1102,7 @@ class UserGoalManager(models.Manager):
         for m in qset:
             userLicenseDict[m.goal.pk].append(m.license)
         for bgoalid, lics in userLicenseDict.items():
-            s = ','.join([sl.pk for sl in lics])
+            s = ','.join([str(sl.pk) for sl in lics])
             logger.info('LicenseBaseGoal {0.pk} : {1}'.format(bgoalid, s))
         return userLicenseDict
 
