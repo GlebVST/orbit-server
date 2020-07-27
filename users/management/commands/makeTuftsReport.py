49f46bd4d97a3b2474f98fb209357e795dc0a547
Faria Chowdhury <faria.chowdhury@gmail.com>
2020-07-27 00:43:04 -0700

    makeTuftsReport: add option for end_of_year report
    TODO: need another cron task that calls the mgmt cmd with
      the --end_of_year option on end of Jun 30  each year.

--- users/management/commands/makeTuftsReport.py
+++ users/management/commands/makeTuftsReport.py
@@ -11,6 +11,16 @@ from users.tuftsutils import MainReport, TUFTS_RECIPIENTS
 
 logger = logging.getLogger('mgmt.tuftqr')
 
+DEV_EMAILS = settings.DEV_EMAILS[0:1]
+MANAGER_EMAILS = [settings.MANAGERS[0][1]]
+
+# reportType (used by email subject)
+REPORT_QUARTER = 'Quarterly'
+REPORT_ANNUAL = 'Annual'
+# used by calcYearReportDateRange
+ANNUAL_REPORT_MONTH = 6
+ANNUAL_REPORT_DAY = 30
+
 # quarterly month ranges
 DATE_RANGE_MAP = {
     1: ((10, 1), (12, 31)), # Q4
@@ -52,6 +62,18 @@ class Command(BaseCommand):
         endDate = datetime(year, e[0], e[1], 23, 59, 59, tzinfo=pytz.utc)
         return (startDate, endDate)
 
+    def calcYearReportDateRange(self, year):
+        """Calculate annual report date range
+        Args:
+            year: int - used for the year of endDate
+        e.g. endDate = 2020-06-30 23:59:59 utc
+         startDate = 2019-06-30 00:00:00 utc
+        Returns tuple: (startDate: datetime, endDate: datetime)
+        """
+        endDate = datetime(year, ANNUAL_REPORT_MONTH, ANNUAL_REPORT_DAY, 23, 59, 59, tzinfo=pytz.utc)
+        startDate = datetime(year-1, ANNUAL_REPORT_MONTH, ANNUAL_REPORT_DAY, tzinfo=pytz.utc)
+        return (startDate, endDate)
+
     def add_arguments(self, parser):
         parser.add_argument(
             '--report_month',
@@ -74,13 +96,21 @@ class Command(BaseCommand):
             default=False,
             help='Only email reports to MANAGERS. Default behavior is to include Tufts recipients in prod env. Test env never includes Tufts recipients.'
         )
+        parser.add_argument(
+            '--end_of_year',
+            action='store_true',
+            dest='end_of_year',
+            default=False,
+            help='If True: all entries from the previous year are included in the report. This option is set by the cron task that should execute on July 1 UTC each year.'
+        )
 
 
-    def createReport(self, options):
+    def createReport(self, startDate, endDate, options):
         """Calculate data and create EmailMessage
         """
-        startDate, endDate = self.calcReportDateRange(options)
-        
+        reportType = REPORT_QUARTER
+        if self.isEndOfYearReport:
+            reportType = REPORT_ANNUAL
         mainReport = MainReport(startDate, endDate,
             settings.GAUTH_SERVICE_CREDENTIALS_FILE,
             settings.GSHEET_TUFTS_EVAL_DOCID,
@@ -101,24 +131,32 @@ class Command(BaseCommand):
         
         # create EmailMessage
         from_email = settings.EMAIL_FROM
-        cc_emails = ['ram@orbitcme.com']
-        bcc_emails = ['faria@orbitcme.com', 'logicalmath333@gmail.com']
+        to_emails = DEV_EMAILS
+        cc_emails = []; bcc_emails = []
         if settings.ENV_TYPE == settings.ENV_PROD:
-            to_emails = ['ram@orbitcme.com']
+            to_emails = MANAGER_EMAILS[:] # make copy before calling extend
             if not options['managers_only']:
                 to_emails.extend(TUFTS_RECIPIENTS)
+            cc_emails = []; bcc_emails = DEV_EMAILS
         else:
             # NOTE: below line is for testing
-            to_emails = ['faria@orbitcme.com']
+            to_emails = DEV_EMAILS
             cc_emails = []; bcc_emails = []
-        subject = "Orbit Quarterly Report ({0}-{1})".format(
+        subject = "Orbit {0} Report ({1}-{2})".format(
+            reportType,
             startReportDate.strftime('%b/%d/%Y'),
             endReportDate.strftime('%b/%d/%Y')
         )
-        startRds = startReportDate.strftime('%b%Y')
-        endRds = endReportDate.strftime('%b%Y')
-        reportFileName = 'orbit-report-{0}-{1}.csv'.format(startRds, endRds)
-        summaryFileName = 'orbit-summary-{0}-{1}.csv'.format(startRds, endRds)
+        if self.isEndOfYearReport:
+            startRds = startReportDate.strftime('%b%d%Y')
+            endRds = endReportDate.strftime('%b%d%Y')
+            reportFileName = 'orbit-annual-report-{0}-{1}.csv'.format(startRds, endRds)
+            summaryFileName = 'orbit-annual-summary-{0}-{1}.csv'.format(startRds, endRds)
+        else:
+            startRds = startReportDate.strftime('%b%Y')
+            endRds = endReportDate.strftime('%b%Y')
+            reportFileName = 'orbit-report-{0}-{1}.csv'.format(startRds, endRds)
+            summaryFileName = 'orbit-summary-{0}-{1}.csv'.format(startRds, endRds)
         
         message = get_template('email/tufts_quarterly_report.html').render(ctx)
         msg = EmailMessage(
@@ -134,15 +172,26 @@ class Command(BaseCommand):
         return msg
 
     def handle(self, *args, **options):
-        # options error check
-        if (options['report_month'] and not options['report_year']) or (options['report_year'] and not options['report_month']):
-            self.stderr.write('If specified, both report_month and report_year must be specified together')
-            return
-        if options['report_month'] and options['report_month'] not in DATE_RANGE_MAP:
-            self.stderr.write('Report month must be one of: 1, 4, 7, or 10')
-            return
         try:
-            msg = self.createReport(options)
+            # options error check
+            if options['end_of_year']:
+                self.isEndOfYearReport = True
+                if not options['report_year']:
+                    now = timezone.now()
+                    year = now.year
+                else:
+                    year = options['report_year']
+                startDate, endDate = self.calcYearReportDateRange(year)
+            else:
+                self.isEndOfYearReport = False
+                if (options['report_month'] and not options['report_year']) or (options['report_year'] and not options['report_month']):
+                    self.stderr.write('If specified, both report_month and report_year must be specified together')
+                    return
+                if options['report_month'] and options['report_month'] not in DATE_RANGE_MAP:
+                    self.stderr.write('Report month must be one of: 1, 4, 7, or 10')
+                    return
+                startDate, endDate = self.calcReportDateRange(options)
+            msg = self.createReport(startDate, endDate, options)
             msg.send()
         except SMTPException as e:
             logger.exception('makeTuftsReport send email failed')
