0c95a7de102afa2483309d8f82f5307f9d6cf45b
Faria Chowdhury <faria.chowdhury@gmail.com>
2020-09-01 16:54:48 -0700

    emailutils: handle profilesToFix (profile with inactive planId)
    mgmt cmd: newUserReport: check for users who never activated a subs
    and their initial selected planId is now inactive. Correct to a
    suitable active public planId if possible, else add profile to
    profilesToFix (must be fixed manually).
    new_user_report template: render list of profilesToFix if exist, and
    instruct admin to correct their profile.planId using the Admin UI.
    mgmt cmd: hpt: check that profile.planId matches latestSubs.planId
    and correct if needed.

--- users/management/commands/newUserReport.py
+++ users/management/commands/newUserReport.py
@@ -4,25 +4,65 @@ from smtplib import SMTPException
 from django.core.management.base import BaseCommand, CommandError
 from django.conf import settings
 from django.utils import timezone
-from users.models import Profile, UserSubscription
+from users.models import Profile, SubscriptionPlan, UserSubscription
 from users.emailutils import sendNewUserReportEmail
 
 logger = logging.getLogger('mgmt.newuser')
 
 
+def checkInactiveProfilePlans():
+    """Find users who never activated a subscription and whose initial selected planId is now inactive
+    Find a corresponding active plan and update their profile.planId.
+    This is done so that in case the user returns to activate their subscription, they will be on
+    an active plan
+    Returns: list of profiles whose planId is inactive but could not be corrected
+    """
+    inactive_plans = SubscriptionPlan.objects.filter(active=False).order_by('pk')
+    planIds = [p.planId for p in inactive_plans]
+    if not planIds:
+        return []
+    planDict = {}
+    tofix = []
+    for p in inactive_plans:
+        # Find an active public plan in the same group as the inactive plan
+        fkwargs = dict(active=True, is_public=True, plan_key=p.plan_key, plan_type=p.plan_type)
+        qs = SubscriptionPlan.objects.filter(**fkwargs).order_by('price','pk')
+        if qs.exists():
+            planDict[p.planId] = qs[0]
+        else:
+            logger.warning('checkInactivePlans: could not find a similar active plan for inactive plan: {0.pk}|{0}'.format(p))
+    # Find users whose profile.planId is set to an inactive plan
+    tofix = []
+    profiles = Profile.objects.filter(planId__in=planIds).order_by('pk')
+    for profile in profiles:
+        user_subs = UserSubscription.objects.getLatestSubscription(profile.user)
+        if not user_subs:
+            # User never started a subscription, but their initial plan selection is now inactive.
+            # Change planId to active public plan in case user decides to activate their subscription later
+            old_planId = profile.planId
+            active_plan = planDict.get(old_planId, None)
+            if active_plan:
+                profile.planId = active_plan.planId
+                profile.save(update_fields=('planId',))
+                logger.info('User {0.pk}|{0} has no subscription. Updated planId from inactive:{1} to active:{0.planId}.'.format(profile, old_planId))
+            else:
+                tofix.append(profile)
+                logger.warning('User {0.pk}|{0} has no subscription. Profile.planId is inactive plan:{0.planId}.'.format(profile))
+    return tofix
+
 class Command(BaseCommand):
-    help = "Finds the list of new users created in the past 24 hours and sends an email report to SALES_EMAIL"""
+    help = "Finds the list of new users created in the past 24 hours and sends an email report to SALES_EMAIL"
 
     def handle(self, *args, **options):
-        now = timezone.now()
-        cutoff = now - timedelta(days=1)
-        profiles = Profile.objects.filter(created__gte=cutoff).order_by('created')
-        email_to = settings.SALES_EMAIL
-        if profiles.count():
-            try:
-                sendNewUserReportEmail(profiles, email_to)
-            except SMTPException as e:
-                logger.exception('New User Report Email to {0} failed'.format(email_to))
-            else:
-                logger.info('New User Report Email to {0} sent.'.format(email_to))
-
+        try:
+            now = timezone.now()
+            # inactive plans check
+            profilesToFix = checkInactiveProfilePlans()
+            cutoff = now - timedelta(days=1)
+            profiles = Profile.objects.filter(created__gte=cutoff).order_by('created')
+            if profilesToFix or profiles.count():
+                sendNewUserReportEmail(profiles, profilesToFix)
+        except SMTPException as e:
+            logger.exception('New User Report Email to {0} failed'.format(email_to))
+        except Exception as e:
+            logger.exception('newUserReport fatal exception')
