a3349d96d07c061e82bf2905073ddcbb8ba24c5f
gleb.starodubtsev <gleb@codeabovelab.com>
2020-10-08 22:24:03 +0700

    Updating db schema

--- users/models/residents.py
+++ users/models/residents.py
@@ -3,49 +3,23 @@ import logging
 from django.contrib.auth.models import User
 from django.db import models
 
+from users.models import SubSpecialty
+
 logger = logging.getLogger('gen.models')
 
-class CaseTagCategory(models.Model):
-    name = models.CharField(help_text='Case tag category name.')
+class TrainingProgramType(models.Model):
+    name = models.CharField(max_length=200, unique=True, help_text='Training program type. Must be unique.')
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
-
-class ProcedureCode(models.Model):
-    code = models.CharField(max_length=200, unique=True, help_text='Procedure code. Must be unique.')
-    created = models.DateTimeField(auto_now_add=True)
-    modified = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return self.code
 
 class TrainingProgram(models.Model):
-    PROGRAM_TYPE_RESIDENCY = 0
-    PROGRAM_TYPE_FELLOWSHIP = 1
-    PROGRAM_TYPE_CHOICES = (
-        (PROGRAM_TYPE_RESIDENCY, 'Residency'),
-        (PROGRAM_TYPE_FELLOWSHIP, 'Fellowship'),
-    )
-    name = models.CharField(max_length=200, unique=True, help_text='Training program name. Must be unique.')
-    type= models.IntegerField(
-        default=1,
-        choices=PROGRAM_TYPE_CHOICES,
-        help_text='Training program type'
-    )
-    created = models.DateTimeField(auto_now_add=True)
-    modified = models.DateTimeField(auto_now=True)
+    name = models.CharField(max_length=200, unique=True, help_text='Training program type. Must be unique.')
+    program_type = models.ForeignKey(TrainingProgramType, null=False, on_delete=models.PROTECT, help_text='Training program type.')
     start_date = models.DateTimeField(null=True, help_text='Start of the program.')
     end_date = models.DateTimeField(null=True, help_text='End of the program.')
-
-    def __str__(self):
-        return self.name
-
-class ProcedureCategory(models.Model):
-    name = models.CharField(max_length=200, unique=True, help_text='Procedure category name. Must be unique.')
-    training_program = models.ForeignKey(TrainingProgram, null=False, on_delete=models.CASCADE, help_text='Training program type for this ontology.')
-    parent = models.ForeignKey('self', null=True, on_delete=models.CASCADE, help_text='Parent category reference for subcategories.')
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
 
@@ -53,23 +27,19 @@ class ProcedureCategory(models.Model):
         return self.name
 
 class Procedure(models.Model):
-    name = models.CharField(max_length=200, unique=True, help_text='Procedure name. Must be unique.')
-    codes = models.ManyToManyField(ProcedureCode,
-                                  blank=True,
-                                  related_name='procedures',
-                                  help_text='Procedure codes.'
-                                  )
-    tag_categories = models.ManyToManyField(CaseTagCategory,
-                                   blank=True,
-                                   related_name='procedures',
-                                   help_text='Case tag categories that apply to this procedure.'
-                                   )
-    procedure_subcategories = models.ManyToManyField(ProcedureCategory,
-                                            blank=True,
-                                            related_name='procedures',
-                                            help_text='Procedure Ontology subcategories that apply to this procedure.'
-                                            )
-
+    PROCEDURE_TYPE_DIAGNOSTIC = 0
+    PROCEDURE_TYPE_INTERVENTION = 1
+    PROCEDURE_TYPE_CHOICES = (
+        (PROCEDURE_TYPE_DIAGNOSTIC, 'Diagnostic'),
+        (PROCEDURE_TYPE_INTERVENTION, 'Intervention'),
+    )
+    name = models.CharField(max_length=200, unique=True, help_text='Orbit Procedure name. Must be unique.')
+    type= models.IntegerField(
+        default=1,
+        choices=PROCEDURE_TYPE_CHOICES,
+        help_text='Procedure type'
+    )
+    regex = models.CharField(max_length=500, unique=True, help_text='Procedure name regex and synonyms.')
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
 
@@ -77,33 +47,26 @@ class Procedure(models.Model):
         return self.name
 
 class TrainingGoal(models.Model):
-    program = models.ForeignKey(TrainingProgram, null=False, on_delete=models.CASCADE, help_text='Training program for this goal.')
+    program_type = models.ForeignKey(TrainingProgramType, null=False, on_delete=models.CASCADE, help_text='Training program for this goal.')
+    subspecialty = models.ForeignKey(SubSpecialty, null=False, on_delete=models.CASCADE, help_text='Subspecialty.')
     procedure = models.ForeignKey(Procedure, null=False, on_delete=models.CASCADE, help_text='Procedure reference for this goal.')
     value = models.IntegerField(default=0, help_text="Goal value.")
 
     class Meta:
-        unique_together = ('program', 'procedure')
+        unique_together = ('program_type', 'subspecialty', 'procedure')
 
 
 class Case(models.Model):
+    npi = models.CharField(max_length=20, unique=True, help_text='NPI number of the resident.')
+    cpt_code = models.CharField(max_length=50, unique=True, help_text='CPT code of the procedure.')
+    procedure = models.ForeignKey(Procedure, null=False, on_delete=models.PROTECT, help_text='Procedure reference for this case.')
     user = models.ForeignKey(User,
                              on_delete=models.CASCADE,
                              related_name='cases',
-                             db_index=True
+                             db_index=True,
+                             help_text='Internal resident user ID.'
                              )
-    resident_id = models.CharField(help_text='External resident ID.')
-    case_code = models.ForeignKey(ProcedureCode, null=False, on_delete=models.CASCADE, help_text='Case Code.')
     timestamp = models.DateTimeField(null=False, help_text='Date and time of the actual procedure.')
     age_category = models.CharField(null=False, max_length=60, help_text='Patient category: child or adult.')
-    tagged = models.DateTimeField(null=True, help_text='Timestamp when resident tagged their case.')
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
-
-class CaseTag(models.Model):
-    case = models.ForeignKey(Case, related_name='tags', on_delete=models.CASCADE, help_text='Case reference.')
-    category = models.ForeignKey(CaseTagCategory, on_delete=models.CASCADE, help_text='Tag category.')
-    value = models.IntegerField(default=0, help_text="Selected tag value")
-    timestamp = models.DateTimeField()
-    created = models.DateTimeField(auto_now_add=True)
-    modified = models.DateTimeField(auto_now=True)
-
