7463f2f847540cde4898ca87eb19ccb2f4bdfcc3
Naoki Eto <naokieto@stanford.edu>
2020-10-31 04:24:01 -0400

    Updated the article discovery stats script so that the study topics are alphabetized

--- scripts/articleDiscoveryStats_dev.py
+++ scripts/articleDiscoveryStats_dev.py
@@ -61,23 +61,22 @@ def main():
         for offer in offers:
             allowed_url = offer.url
             print(allowed_url, allowed_url.studyTopics.all())
-            # only use one study topic so that the percentages add up to 100 percent
+            # only use weighted study topic so that the percentages add up to 100 percent
             if len(allowed_url.studyTopics.all()) > 0:
-                study_topic = allowed_url.studyTopics.all()[0]
-                offer_num_dct[d_user][study_topic.name] += 1
-
+                num_study_topics = len(allowed_url.studyTopics.all())
+                for study_topic in allowed_url.studyTopics.all():
+                    offer_num_dct[d_user][study_topic.name] += 1/num_study_topics
+                    print(d_user.email, study_topic.name.lower())
         # Determine the organization group of this user
         org_member = OrgMember.objects.filter(user=d_user)
         if len(org_member) > 0:
             users_orggroup_dct[d_user] = org_member[0].group
-    #print(num_offers_dct)
-    #print(offer_num_dct)
-    #print(users_orggroup_dct)
 
     num_offers_data = [{'email': user.email,
                         'lastName': user.profile.lastName,
                         'firstName': user.profile.firstName,
                         'totalOffers': offers} for user, offers in num_offers_dct.items()]
+
     for user in num_offers_dct.keys():
         if user.email not in allowed_emails:
             continue
@@ -85,25 +84,42 @@ def main():
         message += "{0} {1}<br>".format(user.profile.firstName, user.profile.lastName)
         if users_orggroup_dct[user]:
             message += "{0}<br>".format(users_orggroup_dct[user])
-        #message += "Great job with your studying! Here's a breakdown of what's happened in the past week: <br>"
         message += "Total number of articles read this week: {0}<br>".format(num_offers_dct[user])
         if len(offer_num_dct[user]) > 0:
             message += "Distribution of topics this week: <br>"
+        message += "<table><tr><th>Study Topic</th><th>Percentage of Offers</th></tr>"
         other = num_offers_dct[user]
+        study_topic_lst = []
         study_topic_offers = []
+
+        for study_topic in StudyTopic.objects.all():
+            study_topic_lst.append((study_topic.name.lower(), study_topic))
+
         for study_topic in offer_num_dct[user]:
             count = offer_num_dct[user][study_topic]
-            study_topic_offers.append((count, study_topic))
             other = other - count
 
         other = max(other, 0)
         if other > 0:
-            study_topic_offers.append((other, "other"))
+            study_topic_lst.append(("other", None))
+
+        # sort alphabetically the study topic list
+        study_topic_lst.sort()
+
         study_topic_offers.sort(reverse=True)
-        for n, s in study_topic_offers:
-            study_topic_percent = round(n * 100/num_offers_dct[user])
-            other = other - n
-            message += "{0}: {1}% <br>".format(s.lower(), study_topic_percent)
+        # Adding the study topics table
+        for topic, study_topic in study_topic_lst:
+            num_offers = num_offers_dct[user]
+            name = ""
+            if topic == "other" and num_offers > 0:
+                study_topic_percent = round(other * 100/num_offers)
+            elif num_offers > 0:
+                study_topic_percent = round(offer_num_dct[user][study_topic.name] * 100/num_offers)
+            else:
+                study_topic_percent = 0
+            message += "<tr><td>{0}</td><td>{1}%</td></tr>".format(topic, study_topic_percent)
+
+        message += "</table>"
 
         message += "-Your Orbit Team <br>"
         message += "To unsubscribe, please email support@orbitcme.com"
--- scripts/articleDiscoveryStats_dev2.py
+++ scripts/articleDiscoveryStats_dev2.py
@@ -28,6 +28,7 @@ def sendEmailBody(user, message, subject, email_addr):
     print('Email sent')
 
 def main():
+
     allowed_emails = ["logicalmath333@gmail.com", "ram+discoverrad@orbitcme.com",\
                       "rsrinivasan02@hotmail.com"]
 
@@ -65,16 +66,14 @@ def main():
             print(allowed_url, allowed_url.studyTopics.all())
             # only use one study topic so that the percentages add up to 100 percent
             if len(allowed_url.studyTopics.all()) > 0:
-                study_topic = allowed_url.studyTopics.all()[0]
-                offer_num_dct[d_user][study_topic.name] += 1
+                num_study = len(allowed_url.studyTopics.all())
+                for study_topic in allowed_url.studyTopics.all():
+                    offer_num_dct[d_user][study_topic.name] += 1/num_study
 
         # Determine the organization group of this user
         org_member = OrgMember.objects.filter(user=d_user)
         if len(org_member) > 0:
             users_orggroup_dct[d_user] = org_member[0].group
-    #print(num_offers_dct)
-    #print(offer_num_dct)
-    #print(users_orggroup_dct)
 
     num_offers_data = [{'email': user.email,
                         'lastName': user.profile.lastName,
@@ -88,26 +87,32 @@ def main():
         message += "{0} {1}<br>".format(user.profile.firstName, user.profile.lastName)
         if users_orggroup_dct[user]:
             message += "{0}<br>".format(users_orggroup_dct[user])
-        #message += "Great job with your studying! Here's a breakdown of what's happened in the past week: <br>"
         message += "Total number of articles read this week: {0}<br>".format(num_offers_dct[user])
-        if len(offer_num_dct[user]) > 0:
-            message += "Distribution of topics this week: <br>"
+        message += "Distribution of topics this week: <br>"
+        message += "<table><tr><th>Study Topic</th><th>Percentage of Offers</th></tr>"
         other = num_offers_dct[user]
-        study_topic_offers = []
-        for study_topic in offer_num_dct[user]:
-            count = offer_num_dct[user][study_topic]
-            study_topic_offers.append((count, study_topic))
-            other = other - count
+        study_topic_lst = []
+        for study_topic in StudyTopic.objects.all():
+            study_topic_lst.append((study_topic.name.lower(), study_topic))
+        # sort alphabetically the study topic list
+        study_topic_lst.sort()
+        tagged_num_study_topics = 0
 
-        other = max(other, 0)
-        tagged_num_study_topics = num_offers_dct[user] - other
+        # get the total count of tagged offers
+        for _, study_topic in study_topic_lst:
+            count = offer_num_dct[user][study_topic.name]
+            tagged_num_study_topics += count
 
-        study_topic_offers.sort(reverse=True)
-        for n, s in study_topic_offers:
-            study_topic_percent = round(n * 100/tagged_num_study_topics)
-            other = other - n
-            message += "{0}: {1}% <br>".format(s.lower(), study_topic_percent)
 
+        # Adding the study topics table
+        for topic, study_topic in study_topic_lst:
+            if tagged_num_study_topics > 0:
+                study_topic_percent = round(offer_num_dct[user][study_topic.name] * 100/tagged_num_study_topics)
+            else:
+                study_topic_percent = 0
+            message += "<tr><td>{0}</td><td>{1}%</td></tr>".format(topic, study_topic_percent)
+
+        message += "</table>"
         message += "-Your Orbit Team <br>"
         message += "To unsubscribe, please email support@orbitcme.com"
         subject='Discovery Weekly Summary ({0} - {1}) Version 2'.format(one_week.strftime("%m/%d"), today.strftime("%m/%d"))
