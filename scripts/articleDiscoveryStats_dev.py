2966c699ef6caee4311e0349984f56cd317b19db
Naoki Eto <naokieto@stanford.edu>
2020-11-03 16:13:44 -0500

    Updated article discovery email statistics so that the reply-to is to support@orbitcme.com change the highlight from light green to light grey Added in stats for most popular topic in org and most popular topic in country capitalized abbreviations reformatted the email text according to Ram's wishes

--- scripts/articleDiscoveryStats_dev.py
+++ scripts/articleDiscoveryStats_dev.py
@@ -25,6 +25,7 @@ def sendEmailBody(user, message, subject, email_addr):
     #to_emails = ['logicalmath333@gmail.com', email_addr]
     to_emails = ['logicalmath333@gmail.com', 'ram@orbitcme.com']
     #to_emails = ['logicalmath333@gmail.com']
+    reply_emails = ['support@orbitcme.com']
 
     msg = EmailMessage(
             subject,
@@ -32,6 +33,7 @@ def sendEmailBody(user, message, subject, email_addr):
             to=to_emails,
             cc=[],
             bcc=[],
+            reply_to=reply_emails,
             from_email=settings.EMAIL_FROM)
     msg.content_subtype = 'html'
     msg.send()
@@ -94,32 +96,65 @@ def main(version):
     # dictionary where key is the user, and the value is the user's organization group
     users_orggroup_dct = collections.defaultdict(lambda : "")
 
+    # used to generate the table with the email/url/study topics (in form of csv)
     articleStudyData = []
+
+    topic_counter = collections.Counter()
+    topic_org_counter = collections.defaultdict(lambda : collections.Counter())
+
     for d_user in discovery_users:
         offers = OrbitCmeOffer.objects.filter(user=d_user, activityDate__range=(one_week, today))
         num_offers_dct[d_user] = len(offers)
-        for offer in offers:
-            allowed_url = offer.url
-            print(allowed_url, allowed_url.studyTopics.all())
-            # only use weighted study topic so that the percentages add up to 100 percent
-            if len(allowed_url.studyTopics.all()) > 0:
-                num_study_topics = len(allowed_url.studyTopics.all())
-                study_topics_names = [s.name.lower() for s in allowed_url.studyTopics.all()]
-                d = {'email': d_user.email, 'url': allowed_url,
-                    'study topics': ", ".join(study_topics_names)}
-                articleStudyData.append(d)
-                for study_topic in allowed_url.studyTopics.all():
-                    offer_num_dct[d_user][study_topic.name] += 1/num_study_topics
-                    print(d_user.email, study_topic.name.lower())
+
         # Determine the organization group of this user
         org_member = OrgMember.objects.filter(user=d_user)
         if len(org_member) > 0:
             users_orggroup_dct[d_user] = org_member[0].group
 
+        # go through all of the offers and the study topics associated with each url/offer
+        for offer in offers:
+            allowed_url = offer.url
+            # only use weighted study topic so that the percentages add up to 100 percent
+            if len(allowed_url.studyTopics.all()) > 0:
+                num_study_topics = len(allowed_url.studyTopics.all())
+                study_topics_names = [s.name.lower() for s in allowed_url.studyTopics.all()]
+                # this is used to generate the table with email/url/study topics
+                d = {'email': d_user.email, 'url': allowed_url,
+                    'study topics': ", ".join(study_topics_names)}
+                articleStudyData.append(d)
+                for study_topic in allowed_url.studyTopics.all():
+                    offer_num_dct[d_user][study_topic.name] += 1/num_study_topics
+
+                    # counters of study topics overall and by org
+                    topic_name = study_topic.name.lower()
+                    topic_counter[topic_name] += 1
+
+                    if d_user in users_orggroup_dct:
+                        topic_org_counter[users_orggroup_dct[d_user]][topic_name] += 1
+
     num_offers_data = [{'email': user.email,
                         'lastName': user.profile.lastName,
                         'firstName': user.profile.firstName,
                         'totalOffers': offers} for user, offers in num_offers_dct.items()]
+
+    topic_abbreviations = set(["ai", "h&n", "gi", "gu", "ir", "msk", "nis"])
+
+    most_popular_topic = [key for key in topic_counter if topic_counter[key] == max(topic_counter.values())]
+    if most_popular_topic:
+        if most_popular_topic[0] in topic_abbreviations:
+            most_popular_topic = most_popular_topic[0].upper()
+        else:
+            most_popular_topic = most_popular_topic[0]
+
+    org_popular_topic = {}
+    print(topic_org_counter)
+    for org, topics in topic_org_counter.items():
+        org_popular_topic[org] = [key for key in topics if topics[key] == max(topics.values())]
+        if org_popular_topic[org]:
+            if org_popular_topic[org][0] in topic_abbreviations:
+                org_popular_topic[org] = org_popular_topic[org][0].upper()
+            else:
+                org_popular_topic[org] = org_popular_topic[org][0]
 
     for user in num_offers_dct.keys():
         if user.email not in allowed_emails:
@@ -147,16 +182,30 @@ def main(version):
             <body>
         """
         message += "Dear {0}, <br>".format(user.profile.firstName)
-        if users_orggroup_dct[user]:
-            message += "Hope all is well at {0}! ".format(users_orggroup_dct[user])
-        message += "Here's your weekly progress report ({0} - {1}) of topics you covered while logged into Orbit on your Chrome browser, iPhone or iPad:<br>"\
-                .format(one_week.strftime("%m/%d"), today.strftime("%m/%d"))
+        message += "Happy Friday. This week in Orbit Discovery, "
+        print(users_orggroup_dct[user])
+        print(org_popular_topic)
+        if users_orggroup_dct[user] and org_popular_topic[users_orggroup_dct[user]]:
+            org = users_orggroup_dct[user]
+            message += "the most popular topic at {0} was {1}, and ".format(org, org_popular_topic[org])
+
+        if most_popular_topic:
+            message += "the most popular topic across the country was {0}. ".format(most_popular_topic)
+        message += "Your summary for the week is below. To capture all of your progress "
+        message += "on service or while studying, stay logged into Orbit - on your Chrome browser, iPhone, or iPad."
+        message += "<ul>"
+        message += '<li>Orbit for Chrome [<a href="https://chrome.google.com/webstore/detail/orbit-for-chrome/ffbnancjlgeeeipcmpiikloifeimgglf">here</a>]</li>'
+        message += '<li>uBlock Origin for Chrome [<a href="https://chrome.google.com/webstore/detail/ublock-origin/cjpalhdlnbpafiamejdnhcphjbkeiagm?hl=en">here</a>]</li>'
+        message += '<li>Orbit for iPhone and iPad [<a href="https://apps.apple.com/us/app/orbit-cme-search-discover/id1524243733">here</a>]</li>'
+        message += "</ul>"
+        message += "Reporting period: {0} - {1}".format(one_week.strftime("%m/%d"), today.strftime("%m/%d"))
         message += "<br>Unique article visits: {0}<br><br>".format(num_offers_dct[user])
 
-        message += "<table><tr><th>Study Topic</th><th>Percent Effort</th></tr>"
+        message += "<table><tr><th>Topic</th><th>Percent Effort</th></tr>"
         other = num_offers_dct[user]
         study_topic_lst = []
         study_topic_offers = []
+
         for study_topic in StudyTopic.objects.all():
             study_topic_lst.append((study_topic.name.lower(), study_topic))
 
@@ -200,8 +249,10 @@ def main(version):
                     study_topic_percent = round(offer_num_dct[user][study_topic.name] * 100/num_offers)
                 else:
                     study_topic_percent = 0
+            if topic in topic_abbreviations:
+                topic = topic.upper()
             if study_topic_percent != 0:
-                message += '<tr bgcolor="lightgreen"><td>{0}</td><td>{1}%</td></tr>'.format(topic, study_topic_percent)
+                message += '<tr bgcolor="lightgrey"><td>{0}</td><td>{1}%</td></tr>'.format(topic, study_topic_percent)
             else:
                 message += '<tr><td>{0}</td><td>{1}%</td></tr>'.format(topic, study_topic_percent)
 
@@ -210,10 +261,12 @@ def main(version):
             </body>
         </html>
         """
-        message += "-Your Orbit Team <br>"
-        message += "To unsubscribe, please email support@orbitcme.com"
+        message += "<br>Best wishes, <br>"
+        message += "Orbit Support <br>"
+        message += "<br>Questions? Please email support@orbitcme.com"
 
-        subject='Discovery Weekly Summary ({0} - {1}) Version {2}'.format(one_week.strftime("%m/%d"), today.strftime("%m/%d"), version)
+        subject='({0} - {1}) Your week in Orbit Discovery'.format(one_week.strftime("%m/%d"), today.strftime("%m/%d"))
+        #subject='Discovery Weekly Summary ({0} - {1}) Version {2}'.format(one_week.strftime("%m/%d"), today.strftime("%m/%d"), version)
         sendEmailBody(user, message, subject, user.email)
 
     attachments = [
